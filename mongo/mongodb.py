1b780140cdcfc0429f08ae855488961c61fcd207
Neozeus81 <alexanderflohr2018@gmail.com>
2025-11-09 21:53:40 -0500

    template added/ connection started

--- mongo/mongodb.py
+++ mongo/mongodb.py
@@ -5,5 +5,13 @@ config = configparser.ConfigParser()
 
 config.read('../config/config.ini')
 
+print(config['Database']['connstring'])
+client = MongoClient(config['Database']['connstring'], tlsAllowInvalidCertificates=True, tlsCertificateKeyFile=config['Database']['pem'])
+db = client[config['Database']['name']]
+col = db[config['Database']['collection']]
+
+temp = {"name":"Hello world", "temp":"ok"}
+
+x = col.insert_one(temp)
 
 
